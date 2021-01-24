ab3f4776e7b136ebfa73356eda6fece24809f02a
Alf149 <rolf@amfelt.dk>
2021-01-24 11:33:51 +0100

    small fixes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,6 +9,8 @@ sudo apt install curl -y
 sudo apt install nano -y
 sudo apt install dnsutils -y
 sudo apt install net-tools -y
-sudo apt install nmap -y
+#sudo apt install nmap -y
 sudo apt install jq -y
-
+sudo apt inststall tree -y
+sudo apt install htop -y
+sudo apt install git-all -y
