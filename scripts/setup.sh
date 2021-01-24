84bdf721e2fe8374386f1182a7d32da7645821fa
Alf149 <rolf@amfelt.dk>
2021-01-24 01:14:54 +0100

    small fixes

---
+++ scripts/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# Author: alf149
+#----------------------------------------------------------------------------
+#
+
+sudo apt update
+sudo apt install curl -y
+sudo apt install nano -y
+sudo apt install dnsutils -y
+sudo apt install net-tools -y
+sudo apt install nmap -y
+sudo apt install jq -y
+
